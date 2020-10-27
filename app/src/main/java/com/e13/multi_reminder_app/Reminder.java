6bcec2aed92ae55d401de737f8badbdaf6fd413f
BlueDragonC45 <55166235+BlueDragonC45@users.noreply.github.com>
2020-10-27 19:35:37 -0400

    Updated new reminder xml to recieve the required data, started reminder creation method

--- app/src/main/java/com/e13/multi_reminder_app/Reminder.java
+++ app/src/main/java/com/e13/multi_reminder_app/Reminder.java
@@ -3,22 +3,30 @@ package com.e13.multi_reminder_app;
 public class Reminder {
 
     public String name;
-//    public ???? time;
+    public int month;
+    public int year;
+    public int day;
+    public int hour;
+    public int minute;
     public int priority;
     public String tier;
     public String frequency;
 
 
-    public Reminder(String name, /*??? time,*/ int priority, String tier, String frequency) {
+    public Reminder(String name, int year, int month, int day, int hour, int minute, int priority, String tier, String frequency) {
         this.name = name;
-//        this.time = time;
+        this.month = month;
+        this.year = year;
+        this.day = day;
+        this.hour = hour;
+        this.minute = minute;
         this.priority = priority;
         this.tier = tier;
         this.frequency = frequency;
     }
 
     public Reminder getById(int id) {
-        Reminder rmd = new Reminder(name, /*time,*/ priority, tier, frequency);
+        Reminder rmd = new Reminder(name, year, month, day, hour, minute, priority, tier, frequency);
         return rmd;
     }
 
--- app/src/main/java/com/e13/multi_reminder_app/ReminderCreationActivity.java
+++ app/src/main/java/com/e13/multi_reminder_app/ReminderCreationActivity.java
@@ -2,12 +2,19 @@ package com.e13.multi_reminder_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class ReminderCreationActivity extends AppCompatActivity {
+
+    private int priority = -1;
+    private String tier;
+    private String frequency;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +26,78 @@ public class ReminderCreationActivity extends AppCompatActivity {
         viewReminder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ReminderCreationActivity.this, MainPageActivity.class));
+                finish();
             }
         });
+
+        final Button createReminder = findViewById(R.id.nr_create);
+        final TextView name = findViewById(R.id.editTextName);
+        final DatePicker datePicker = findViewById(R.id.createAlarm_datePicker);
+        final TimePicker timePicker = findViewById(R.id.createAlarm_timePicker);
+
+        createReminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (name.length() == 0) {
+                    Toast.makeText(getApplicationContext(), "You need to pick a name!", Toast.LENGTH_LONG).show();
+                    return;
+                } else if (priority == -1) {
+                    Toast.makeText(getApplicationContext(), "You need to pick a priority!", Toast.LENGTH_LONG).show();
+                    return;
+                } else if (tier == null) {
+                    Toast.makeText(getApplicationContext(), "You need to pick a tier!", Toast.LENGTH_LONG).show();
+                    return;
+                } else if (frequency == null) {
+                    frequency = "NEVER";
+                }
+                Reminder rmd = new Reminder(name.toString(), datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth(),
+                        timePicker.getHour(), timePicker.getMinute(), priority, tier, frequency);
+            }
+        });
+
+    }
+
+    public void onClickRadioButton(View view) {
+        int pressId = view.getId();
+
+        switch (pressId) {
+            case R.id.radioUrgent:
+                priority = 4;
+                break;
+            case R.id.radioHigh:
+                priority = 3;
+                break;
+            case R.id.radioMed:
+                priority = 2;
+                break;
+            case R.id.radioLow:
+                priority = 1;
+                break;
+            case R.id.radioMacro:
+                tier = "MACRO";
+                break;
+            case R.id.radioMeso:
+                tier = "MESO";
+                break;
+            case R.id.radioMicro:
+                tier = "MICRO";
+                break;
+            case R.id.radioNever:
+                frequency = "NEVER";
+                break;
+            case R.id.radioHour:
+                frequency = "HOUR";
+                break;
+            case R.id.radioDay:
+                frequency = "DAY";
+                break;
+            case R.id.radioWeek:
+                frequency = "WEEK";
+                break;
+            case R.id.radioMonth:
+                frequency = "MONTH";
+                break;
+        }
     }
 
 }
