4ef3b45b5a46130bf88ee7248c69e0161c029a73
BlueDragonC45 <55166235+BlueDragonC45@users.noreply.github.com>
2020-11-01 11:54:52 -0500

    Fix for connection pool closed error

--- app/src/main/java/com/e13/multi_reminder_app/DatabaseHelper.java
+++ app/src/main/java/com/e13/multi_reminder_app/DatabaseHelper.java
@@ -89,7 +89,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public Cursor getAllData() {
         SQLiteDatabase db = this.getWritableDatabase();
         Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
-        db.close();
         return res;
     }
 
